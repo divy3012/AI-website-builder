0cf85b78187444387e92905df1e8dead9ed141ee
Divy Suthar <divy301203@gmail.com>
2026-07-28 14:39:13 +0530

    openRouter file updated

--- Backend/config/openRouter.js
+++ Backend/config/openRouter.js
@@ -18,7 +18,7 @@ export const generateResponse = async (prompt) => {
         },
       ],
       temperature: 0.2,
-      max_token: 3000,
+      max_tokens: 3000,
     }),
   });
   if (!res.ok) {
